e6e8dcf08b571e4b28749f0c942157fe77de7850
Jonas Wagner <jwagner@raptus.com>
2012-11-01 17:49:53 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0a1'
 setup(name='raptus.supersized',
       version=version,
       description="Provides the supersized jQuery plugin integrated into Plone",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
